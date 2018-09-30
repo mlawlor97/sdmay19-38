f26c336328c5ba1b9041304e68f7b252b6c84aca
Mitchell Kerr <mrkerr@iastate.edu>
2018-09-30 17:48:41 -0500

    Goes to all apps now, still need to check more on versions

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import requests
-import os.path
 from bs4 import BeautifulSoup
-
-
+import time
+import winsound
 
 def getDevPages(url):
-    r = requests.get(url + "/developers/")
+    time.sleep(1)
+    r = requests.get(url)
     soup = BeautifulSoup(r.content, "lxml")  # there are faster parsers
+    urlforPaste = "https://www.apkmirror.com"
 
     pagesToVisit = []
 
@@ -18,13 +19,15 @@ def getDevPages(url):
             for innerel in elements.find_all("h5", {"class": "appRowTitle wrapText marginZero block-on-mobile"}):
                 a = innerel.find("a", {"class": "fontBlack"})
                 goHere = a.get('href')
-                pagesToVisit.append(url + goHere)
+                pagesToVisit.append(urlforPaste + goHere)
     return pagesToVisit
 
 
 def openPages(url):
+    time.sleep(1)
     r = requests.get(url)
     soup = BeautifulSoup(r.content, "lxml")  # there are faster parsers
+    urlforPaste = "https://www.apkmirror.com"
     morePagesToVisit = []
     version = soup.find_all("div", {"id": "primary"})
 
@@ -35,25 +38,34 @@ def openPages(url):
                     for innerel in w.find_all("h5", {"class": "appRowTitle wrapText marginZero block-on-mobile"}):
                         a = innerel.find("a", {"class": "fontBlack"})
                         goHere = a.get('href')
-                        morePagesToVisit.append(url + goHere)
+                        morePagesToVisit.append(urlforPaste + goHere)
     return morePagesToVisit
 
 def main():
-    url = "https://www.apkmirror.com"
+    url = "https://www.apkmirror.com/developers/"
 
     f = open("ApkMirror Crawler Output.txt", "w")
     f.write("")
     f.close()
-
-    pagesToVisit = getDevPages(url)
     outFile = "ApkMirror Crawler Output.txt"
     f = open(outFile, "a")
-    for link in pagesToVisit:
-        o = openPages(link)
-        for l in o:
-            f.write(l)
-            f.write("\n")
-
+    i=1
+    pagesToVisit = []
+    while i<28:
+        if i == 1:
+            pagesToVisit = getDevPages(url)
+        else:
+            istr = str(i)
+            pagesToVisit = getDevPages(url+"page/"+istr+"/")
+        for link in pagesToVisit:
+            o = openPages(link)
+            for l in o:
+                f.write(l)
+                f.write("\n")
+        i+=1
+        duration = 1000  # millisecond
+        freq = 440  # Hz
+        winsound.Beep(freq, duration)
     f.close()
 
 
