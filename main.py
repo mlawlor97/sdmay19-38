aa5fced2d31abb0cfb57931995de81d06d43fb3e
Mitchell Kerr <mrkerr@iastate.edu>
2018-10-01 00:27:49 -0500

    Crawling 99% of apps, getting titles

--- main.py
+++ main.py
@@ -23,6 +23,41 @@ def getDevPages(url):
     return pagesToVisit
 
 
+
+def getDataTitle(url):
+    time.sleep(1)
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "lxml")  # there are faster parsers
+    headerContents = soup.find("div", {"class": "site-header-contents"})
+
+    t = headerContents.find("h1", {"class": "marginZero wrapText app-title fontBlack noHover"})
+    return t.text
+
+
+
+
+
+
+
+def seeMoreUploads(url):
+    time.sleep(1)
+    r = requests.get(url)
+    urlforPaste = "https://www.apkmirror.com"
+    soup = BeautifulSoup(r.content, "lxml")  # there are faster parsers
+    morePagesToVisit = []
+    i = 1
+    version = soup.find_all("div", {"id": "primary"})
+    for elements in version:
+        for w in elements.find_all("div", {"class": "listWidget"}):
+            for d in w.find_all("div", {"class": "appRow"}):
+                if i > 10:
+                    if d.find("a", {"class": "fontBlack"}):
+                        a = d.find("a", {"class": "fontBlack"})
+                        hr = a.get('href')
+                        morePagesToVisit.append(urlforPaste+hr)
+                i+=1
+    return morePagesToVisit
+
 def openPages(url):
     time.sleep(1)
     r = requests.get(url)
@@ -39,6 +74,12 @@ def openPages(url):
                         a = innerel.find("a", {"class": "fontBlack"})
                         goHere = a.get('href')
                         morePagesToVisit.append(urlforPaste + goHere)
+                    for innerel2 in w.find_all("a", {"class": "fontBlack"}):
+                        if innerel2.text == "See more uploads...":
+                            moreuploads = innerel2.get("href")
+                            uArr = seeMoreUploads(urlforPaste+moreuploads)
+                            for u in uArr:
+                                morePagesToVisit.append(u)
     return morePagesToVisit
 
 def main():
@@ -60,7 +101,8 @@ def main():
         for link in pagesToVisit:
             o = openPages(link)
             for l in o:
-                f.write(l)
+                tit = getDataTitle(l)
+                f.write(tit)
                 f.write("\n")
         i+=1
         duration = 1000  # millisecond
