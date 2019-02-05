420cd5765ae3eee235e244b233c2c74a5878816b
Matt Lawlor <mattlawlor@mattmacbook-pro.student.iastate.edu>
2019-02-05 15:31:51 -0600

    start frontend

---
+++ csafe-frontend/src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function SearchBar(props) {
+  return (
+    <form>
+      <label>
+        Name:
+        <input type="text" name="name" />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  )
+}
+
+export default SearchBar
