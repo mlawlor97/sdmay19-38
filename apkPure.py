3bd1977d662b49de939ba28b619ca1d809d0a2d4
Connor <ckoco1@iastate.edu>
2019-02-10 18:58:00 -0600

    updated apkPure

--- apkPure.py
+++ apkPure.py
@@ -1,223 +1,248 @@
 from utils import RateLimiter, requestHTML, logToFile, removeSpecialChars, createPath, downloadApk, writeOutput
 from utils import writeAppDB, writeVersionDB  # DB Connectors
 from SupportFiles.webDriverUtils import WebDriver
-from SupportFiles.metaDataBase import DataCollectionBase
-from SupportFiles.crawlerBase import CrawlerBase, ThreadHelper
+from SupportFiles.metaDataBase2 import DataCollectionBase
+from SupportFiles.crawlerBase import CrawlerBase
 
-from threading import Thread
+from threading import Thread, Lock
+from datetime import datetime, timedelta
+from time import sleep
+
 
 class GetPureData(DataCollectionBase):
 
-    def getDeveloper(self):
-        self.tryCollection('Developer', lambda: self.soup.find(class_="details-author").find('a').text.strip())
+    def rating(self):
+        rating = self.soup.find(class_='rating')
+        return rating.text.strip() if rating else 0.0
 
-    def getPackage(self):
-        self.tryCollection('Package', lambda: self.url.split('/')[-1])
-
-    def getCategory(self):
-        self.tryCollection('Category', lambda: self.soup.find(class_="additional")('span')[-1].text.strip())
-
-    def getDescription(self):
-        self.tryCollection('Description', lambda: self.soup.find(class_="content").text)
-
-    def getRating(self):
-        rating = self.soup.find(class_="rating")
-        self.tryCollection('Rating', lambda: rating.text.strip() if rating else 0.0)
-
-    def getTags(self):
-        self.tryCollection('Tags', lambda: [tag.text for tag in self.soup.find(class_="tag_list")('li') if tag.text])
+    _getDev     = ("Developer",     lambda _: _.soup.find(class_='details-author').find('a').text.strip())
+    _getPkg     = ("Package",       lambda _: _.url.split('/')[-1])
+    _getCat     = ("Category",      lambda _: _.soup.find(class_='additional')('span')[-1].text.strip())
+    _getDesc    = ("Description",   lambda _: _.soup.find(class_='content').text)
+    _getRating  = ("Rating",        lambda _: _.rating())
+    _getTags    = ("Tags",          lambda _: [tag.text for tag in _.soup.find(class_="tag_list")('li') if tag.text])
 
 
 class GetPureVersion(DataCollectionBase):
 
-    def getType(self):
-        self.tryCollection('Apk Type', lambda: self.soup.find(class_="ver-item-t").text.strip())
-        self.soup = self.soup.find(class_="ver-info-m")
+    def changeLog(self):
+        log = self.soup.find(class_='ver-whats-new')
+        return log.text if log and log.text.strip() != '' else "No Patch Notes"
 
-    def getFileSize(self):
-        self.tryCollection('File Size', lambda: str(self.soup.find('strong', string="File Size: ").next_sibling))
-
-    def getRequirements(self):
-        self.tryCollection('Requirements', lambda: str(self.soup.find('strong', string="Requires Android: ").next_sibling))
-
-    def getPublishDate(self):
-        self.tryCollection('Publish Date', lambda: str(self.soup.find('strong', string="Update on: ").next_sibling))
-
-    def getSignature(self):
-        self.tryCollection('Signature', lambda: str(self.soup.find('strong', string="Signature: ").next_sibling).strip())
-
-    def getSHA(self):
-        self.tryCollection('SHA', lambda: str(self.soup.find('strong', string="File SHA1: ").next_sibling))
-
-    def getPatchNotes(self):
-        patchNotes = self.soup.find(class_='ver-whats-new')
-        self.tryCollection('Patch Notes', lambda: patchNotes.text) if patchNotes else None
+    _getType    = ("Apk Type",      lambda _: _.soup.find(class_='ver-item-t').text)
+    _getSize    = ("File Size",     lambda _: str(_.soup.find(class_='ver-item-s').text))
+    _getReqs    = ("Requirements",  lambda _: str(_.soup.find('strong', string='Requires Android: ').next_sibling))
+    _getPubDate = ("Publish Date",  lambda _: str(_.soup.find(class_='update-on').text))
+    _getPatch   = ("Patch Notes",   lambda _: _.changeLog())
+    _getSign    = ("Signature",     lambda _: str(_.soup.find('strong', string='Signature: ').next_sibling).strip())
+    _getSha     = ("SHA",           lambda _: str(_.soup.find('strong', string='File SHA1: ').next_sibling).strip())
 
 
+# noinspection PyCompatibility
 class GetPureReview(DataCollectionBase):
 
-    def getUser(self):
-        self.tryCollection('User', lambda: self.soup.find(class_="author-name").find('span').text.strip())
-        return None if self.metaData.get('User') else TypeError
+    def pubDate(self):
+        now = datetime.now()
+        date = self.soup.find(class_='author-time').find('span').text.strip()
+        if 'ago' in date:
+            timePast = date.split(' ')[0]
+            timePast = int(timePast) if 'a' not in timePast else 1
+            now -= timedelta(days=timePast) if 'days' in date else timedelta(hours=timePast)
+        return now.strftime("%Y-%m-%d")
 
-    def getTitle(self):
-        self.tryCollection('Title', lambda: self.soup.find(class_="article-title").text.strip(), True)
+    def message(self):
+        article = self.soup.find(class_='article')
+        return ''.join(self._msg(article, []))
 
-    def getPublishDate(self):  # TODO Needs work for ...ago times
-        self.tryCollection('Publish Date', lambda: self.soup.find(class_="author-time").find('span').text.strip())
+    def _msg(self, soup, msg: list):
+        for content in soup.contents:
+            if content.string:
+                msg.append(content.string)
+            else:
+                alt, href, vidSrc = content.get('alt'), content.get('href'), content.get('data-src')
+                if alt:
+                    msg.append(alt)
+                elif vidSrc:
+                    msg.append(f"[{vidSrc}]")
+                elif href:
+                    msg.append(f"[{href}]")
+                self._msg(content, msg)
+        return msg
 
-    def getReview(self):  # TODO Get links
-        emotes = []
-        [emotes.append(emote['alt']) for emote in self.soup.find(class_="article")(class_="emojione")]
-        self.tryCollection('Message', lambda: self.soup.find(class_="article").text.strip() + ''.join(emotes))
+    def msgRating(self):
+        rating = self.soup.find(class_='cmt-vote')
+        return 0 if 'Like' in rating.text else rating.text.strip()
+    
+    def responses(self, soup, resps: list):
+        soup = soup.find('ul', recursive=False)
+        lastSlash = self.url.rfind('/') + 1
+        if not soup:
+            return
+        for response in soup(id=True, recursive=False):
+            data = GetPureReview(soup=requestHTML(self.url[:lastSlash] + response['id'].lstrip('c'))).getAll()
+            resps.append(data.metaData)
+            self.responses(response, resps)
+        if resps.__len__() is 0:
+            raise AttributeError
+        else:
+            return resps
 
-    def getMessageRating(self):
-        rating = self.soup.find(class_="cmt-vote")
-        self.tryCollection('Message Rating', lambda: 0 if 'Like' in rating.text else int(rating.text.strip()))
+    _getUser    = ("User",           lambda _: _.soup.find(class_='author-name').find('span').text.strip())
+    _getPubDate = ("Publish Date",   lambda _: _.pubDate())
+    _getTitle   = ("Title",          lambda _: _.soup.find(class_='article-title').text.strip(), False)
+    _getReview  = ("Message",        lambda _: _.message().strip())
+    _getMsgRate = ("Message Rating", lambda _: _.msgRating())
+    _getReplys  = ("Responses",      lambda _: _.responses(_.soup.find(id="cmt-reply-data"), []), False)
 
 
 # noinspection PyCompatibility
 class ApkPure(CrawlerBase):
 
-    def __init__(self, siteUrl="https://apkpure.com", rateLimiter=RateLimiter()):
-        super().__init__(siteUrl, rateLimiter)
-        self.categoryLinks  = []
-        self.threads        = []
+    siteUrl = "http://apkpure.com"
+    rateLimiter = RateLimiter(0, 0)
+
+    def __init__(self, siteUrl=siteUrl, limiter=rateLimiter):
+        super().__init__(siteUrl, limiter)
+        self.webDriver = WebDriver()
+        self.lock = Lock()
+        self.categories = []
 
     def crawl(self):
-        def categoryThread(pure, category):
-            tHelper = ThreadHelper(driver=WebDriver())
-            pageNumber = 1
-            while pure.getAppsOnPage(pure.siteUrl + category + '?page=' + str(pageNumber), tHelper):
-                pageNumber += 1
-            print('Finished: ' + category)
+        cats = self.getCategories()
+        threads = []
 
-        self.getCategories()
-        for category in self.categoryLinks:
-            self.threads.append(Thread(target=categoryThread, args=(self, category, )))
+        for cat in cats[0:2]:
+            threads.append(Thread(target=self._crawlCategory, args=(cat, )))
+            threads[-1].start()
 
-        print(self.threads.__len__())
-        [t.start() for t in self.threads]
-        [t.join() for t in self.threads]
+        [t.join() for t in threads]
+                
+    def getCategories(self, list_=[]):
+        cats = requestHTML(f"{self.siteUrl}/app")(class_='index-category')
+        [[list_.append(cat['href']) for cat in sect(href=True)] for sect in cats]
+        return list_
 
-    @staticmethod
-    def loadPage(driver, url, verifier):
-        driver.loadPage(url, 'class name', verifier)
-        driver.driver.execute_script('policy_review.setReview();')  # Gets rid of cookie agreement
-        driver.driver.execute_script('document.getElementById("ad-aegon-side").remove()') # Removes QR code
-    
-    def getCategories(self):
-        cats = requestHTML(self.siteUrl + '/app')(class_="index-category")
-        [[self.categoryLinks.append(cat['href']) for cat in sect(href=True)] for sect in cats]
+    def _crawlCategory(self, category, pageNumber=1):
+        while self._getAppsOnPage(f"{self.siteUrl}{category}?page={pageNumber}"):
+            pageNumber += 1
+        print(f"Finished: {category}")
 
-    def getAppsOnPage(self, url, tHelper):
+    def _getAppsOnPage(self, url):
         apps = requestHTML(url)(class_='category-template-title')
         for app in apps:
-            tHelper.url = self.siteUrl + app.find('a', href=True)['href']
-            self.scrapeAppData(requestHTML(tHelper.url), tHelper)
+            currUrl = self.siteUrl + app.find('a', href=True)['href']
+            appName, id_ = self._scrapeAppData(currUrl)
 
-            if tHelper.app:
-                tHelper.url += '/versions'
-                self.collectAllVersions(tHelper)
-                # self.collectAllReviews(tHelper)
+            if appName:
+                self._collectAllVersions(id_, currUrl + '/versions')
+                # self._collectAllReviews()
+            return None
 
-        return None if apps.__len__() is 0 else not None
+        return None if apps.__len__() == 0 else not None
 
-    def scrapeAppData(self, soup, tHelper):
+    def _scrapeAppData(self, url):
+        soup = requestHTML(url)
         try:
-            tHelper.app = soup.find(class_='title-like').find('h1').text.strip()
+            appName = soup.find(class_='title-like').find('h1').text.strip()
         except AttributeError:
-            tHelper.app = None
+            appName = None
+            return
 
-        pureData = GetPureData(tHelper.url, soup).getAll()
+        pureData = GetPureData(url, soup).getAll()
 
-        id = writeAppDB('ApkPure', tHelper.app, pureData.metaData)
-        print(id)
+        id_ = writeAppDB('ApkPure', appName, pureData.metaData)
+        print(id_)
 
-    def collectAllVersions(self, tHelper):
-        soup = requestHTML(tHelper.url)
+        return (appName, id_)
+
+    def _collectAllVersions(self, id_, url):
+        soup = requestHTML(url)
         versionList = soup.find(class_='ver')('li')
+
         if 'Page Deleted' not in soup.title.text:
-            self.scrapeVersions(versionList, tHelper)
+            self._scrapeVersions(id_, url, versionList)
+            pass
         else:
-            logToFile('Versions.txt', tHelper.url + '\n')
+            logToFile('Versions.txt', f"{url}\n")
 
-    def scrapeVersions(self, versionList, tHelper):
+    def _scrapeVersions(self, id_, url, versionList):
         index = 0
-        self.loadPage(tHelper.driver, tHelper.url, 'ver')
 
+        if versionList[0].find(class_="ver-whats-new"):
+            self.lock.acquire() 
+            self._loadPage(url, 'ver')
+            for v in versionList:
+                self.webDriver.clickPopUp("ver-item-m", "mfp-close", index, False)
+                self.webDriver.clickAway("mfp-close", "class name")
+                index += 1
+
+            sleep(0.1)
+            v = self.webDriver.fetchPage()
+            while 'loading..' in v.find(class_='ver-whats-new'):
+                v = self.webDriver.fetchPage()
+            versionList = v.find(class_='ver')('li')
+            self.lock.release()
+        
         for v in versionList:
-            if v.find(class_="ver-whats-new"):
-                v = tHelper.driver.clickPopUp("ver-item-m", "mfp-close", index=index)
-                while 'loading..' in v.find(class_='ver-whats-new').text:
-                    v = tHelper.driver.fetchPage()
-                tHelper.driver.clickAway("mfp-close", "class name")
-                v = v.find(class_='ver-wrap')('li')[index]
-                
             version = v.find('span').text.lstrip('V')
-            pureVersion = GetPureVersion(tHelper.url, v).getAll()
-            writeVersionDB('ApkPure', app, version, pureVersion.metaData)
+            pureVersion = GetPureVersion(url, v).getAll()
 
-            index += 1
-            # self.scrapeApk(self.siteUrl + v.find(href=True)['href'], './apks/')  # TODO Uncomment when collecting APKs
+            writeVersionDB("ApkPure", id_, version, pureVersion.metaData)
+            self.scrapeApk(f"{self.siteUrl}{v.find(href=True)['href']}", './apks/')
 
-    def collectAllReviews(self, tHelper):
-        groupName = removeSpecialChars(tHelper.app.lower()).replace(' ', '-')
-        reviewsUrl = self.siteUrl + '/group/' + groupName + '?reviews=1&page='
-        tHelper.url = self.siteUrl + '/group/' + groupName + '/'
+    def _collectAllReviews(self, appName):
+        groupName = removeSpecialChars(appName.lower()).replace(' ', '-').rstrip('-')
+        reviewsUrl = f"{self.siteUrl}/group/{groupName}?reviews=1&page="
+        baseUrl = f"{self.siteUrl}/group/{groupName}/"
 
         pageNumber = 1
-        while self.scrapeReviews(reviewsUrl + pageNumber.__str__(), tHelper):
+        while self._scrapeReviews(f"{reviewsUrl}{pageNumber}", baseUrl):
             pageNumber += 1
 
-    def scrapeReviews(self, url, tHelper):
+    def _scrapeReviews(self, groupUrl, url):
         reviews = requestHTML(url)('li', class_='cmt-root')
         for review in reviews:
-            self.loadPage(tHelper.driver, tHelper.url + review['id'].lstrip('c'), 'bread-crumbs')
-            tHelper.driver.loadMore('cmt-more-btn')
-            soup = tHelper.driver.fetchPage()
+            reviewUrl = groupUrl + review['id'].lstrip('c')
+            self.lock.acquire()
+            self._loadPage(reviewUrl, 'bread-crumbs')
+            self.webDriver.loadMore('cmt-more-btn')
+            soup = self.webDriver.fetchPage()
+            self.lock.release()
 
-            reviewData = GetPureReview(url, soup.select_one('.author')).getAll()
-            reviewData.tryCollection('Rating', lambda: review.select_one('.cmt-icon-s-stars')['data-score'], True)
-            reviewData.tryCollection('Responses', lambda: self.scrapeResponses(soup.find(id="cmt-reply-data"), [], tHelper.url))
+            reviewData = GetPureReview(reviewUrl, soup).getAll()
+            reviewData.tryCollection("Rating", 
+                lambda _: review.select_one('.cmt-icon-s-strs')['data-score'], False)
 
-            publishDate, user = reviewData.metaData.get('Publish Date'), reviewData.metaData.get('User').replace('/', '')
-            destination = './reviews/' + publishDate + '~' + user + '.txt'  # TODO Better naming convention???
-            writeOutput(destination=destination, dataDict=reviewData.metaData)
+            pubDate = reviewData.metaData.get('Publish Date')
+            user = reviewData.metaData.get('User').replace('/', '')
+            destination = f"./reviews/{pubDate}~{user}.txt"
+            writeOutput(destination, dataDict=reviewData.metaData)
 
-        return None if reviews.__len__() is 0 else not None
-
-    @staticmethod
-    def scrapeResponses(soup, responses, url):
-        soup = soup.find('ul', recursive=False)
-        if not soup:
-            return []
-        for response in soup(id=True, recursive=False):
-            data = GetPureReview(soup=requestHTML(url + response['id'].lstrip('c'))).getAll()
-            data.tryCollection('Responses', lambda: ApkPure.scrapeResponses(response, [], url))
-            responses.append(data.metaData)
-        return responses
+        return None if reviews.__len__() == 0 else not None
 
     @staticmethod
     def scrapeApk(downloadUrl, savePath):
-        soup = requestHTML(url=downloadUrl)
+        soup = requestHTML(downloadUrl)
+        downloadLink = soup.find(id='download_link')
 
-        downloadLink = soup.find(id="download_link")
         if downloadLink:
-            fileName = soup.find(class_="file").text.strip()  # TODO Change to better naming convention
-
+            fileName = soup.find(class_='file').text.strip()
             fullPath = createPath(fileName, savePath)
-            downloadApk(downloadLink.get('href'), fullPath, fileName, savePath.split('/')[-1])
-        else:
-            logToFile('APKCheck.txt', downloadUrl + '\n')
+            savePath = savePath.split('/')[-1]
+            downloadApk(downloadLink.get('href'), fullPath, fileName, savePath)
+
+    def _loadPage(self, url, verifier, searchBy="class name"):
+        self.webDriver.loadPage(url, searchBy, verifier)
+        self.webDriver.driver.execute_script('policy_review.setReview();')  # Gets rid of cookie agreement
+        self.webDriver.driver.execute_script('document.getElementById("ad-aegon-side").remove()')  # Removes QR code
 
 
 def main():
     try:
         ApkPure().crawl()
     except KeyboardInterrupt:
-        print('Ended Early')
-    print('Finished')
+        print("Ended Early")
+    print("Finished")
 
 
 if __name__ == '__main__':
