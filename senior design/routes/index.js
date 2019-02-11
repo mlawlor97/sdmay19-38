c44c641de542c5c9fcfb9a8e82d8e7ef458d6d3f
Emmett Kozlowski <emmettk@iastate.edu>
2019-02-11 16:54:20 -0600

    push from server

--- senior design/routes/index.js
+++ senior design/routes/index.js
@@ -4,8 +4,9 @@ let ApplicationModel = require('../models/application');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  ApplicationModel.find({
-        app_name: req.body.keyword
+  console.log(req.query.keyword);
+	ApplicationModel.find({
+        app_name: req.query.keyword
       })
       .then(doc => {
           console.log(doc);
