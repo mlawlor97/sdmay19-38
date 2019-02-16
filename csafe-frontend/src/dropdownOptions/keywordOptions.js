384b0424643bb39493df00ea62c9ab488cb02e86
Matt Lawlor <mattlawlor@mattmacbook-pro.student.iastate.edu>
2019-02-15 20:48:29 -0600

    add report values

---
+++ csafe-frontend/src/dropdownOptions/keywordOptions.js
@@ -0,0 +1,11 @@
+const keywordOptions = [
+  {
+    label: 'Keywords',
+    options: [
+      { value: 'appName', label: 'App Name', group: 'Keywords' },
+      { value: 'packageName', label: 'Package Name', group: 'Keywords' }
+    ]
+  }
+]
+
+export default keywordOptions
