afcca52f5d11970558cab8d1125a0fc571d94baa
emmettk <emmettk@iastate.edu>
2019-04-24 14:08:33 -0500

    made changes to driver

--- SupportFiles/webDriverUtils.py
+++ SupportFiles/webDriverUtils.py
@@ -16,10 +16,13 @@ class WebDriver:
 
         options = webdriver.ChromeOptions()
         options.add_argument('--headless')
+        options.add_argument("--disable-extensions")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
 
         self.driver = webdriver.Chrome(
             chrome_options=options,
-            executable_path=createPath("SupportFiles", "chromedriver")
+            executable_path='/usr/bin/chromedriver'
         )
 
         self.Url = None
