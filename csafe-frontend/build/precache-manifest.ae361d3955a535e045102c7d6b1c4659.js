5a5c22b93ffdcfaa3488d8d23ce6da82310e4c38
root <root@sdmay19-38.ece.iastate.edu>
2019-04-26 18:16:06 -0500

    build

---
+++ csafe-frontend/build/precache-manifest.ae361d3955a535e045102c7d6b1c4659.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e61f5be86114c7f278b4",
+    "url": "/static/js/main.e61f5be8.chunk.js"
+  },
+  {
+    "revision": "8d2a8c07b929bad6febf",
+    "url": "/static/js/1.8d2a8c07.chunk.js"
+  },
+  {
+    "revision": "e61f5be86114c7f278b4",
+    "url": "/static/css/main.967436aa.chunk.css"
+  },
+  {
+    "revision": "48caa8da0af1a4f81a660389b1c9b69d",
+    "url": "/index.html"
+  }
+];
